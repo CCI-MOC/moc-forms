4745223895ce83e321a63a1cf149449e5c4f2fa6
Kristi Nikolla <kristi@nikolla.me>
2019-03-17 13:25:45 -0400

    added oidc

--- moc_forms/app.py
+++ moc_forms/app.py
@@ -1,5 +1,8 @@
+import logging
+
 import flask
 import flask_bootstrap
+import flask_oidc
 import flask_wtf
 import wtforms
 from wtforms import validators
@@ -8,10 +11,22 @@ from moc_forms import adjutant
 from moc_forms import config
 
 app = flask.Flask(__name__)
-app.config['SECRET_KEY'] = 'helloworld'
+
+app.config.update({
+    'SECRET_KEY': 'SomethingNotEntirelySecret',
+    'TESTING': True,
+    'DEBUG': True,
+    'OIDC_CLIENT_SECRETS': 'secrets/client_secrets.json',
+    'OIDC_ID_TOKEN_COOKIE_SECURE': False,
+    'OIDC_REQUIRE_VERIFIED_EMAIL': False,
+})
+
+oidc = flask_oidc.OpenIDConnect(app)
 flask_bootstrap.Bootstrap(app)
 
 CONF = config.CONF
+
+logging.basicConfig(level=logging.DEBUG)
 
 
 class SignUpForm(flask_wtf.FlaskForm):
@@ -35,6 +50,8 @@ class SignUpForm(flask_wtf.FlaskForm):
     project_services = wtforms.SelectMultipleField(
         'Services',
         choices=[('openstack-kaizen', 'OpenStack Kaizen')],
+        default=('openstack-kaizen', 'OpenStack Kaizen'),
+        render_kw={'readonly': True},
         validators=[validators.DataRequired()]
     )
     project_moc_sponsor = wtforms.StringField('MOC Sponsor for Project',
@@ -43,19 +60,33 @@ class SignUpForm(flask_wtf.FlaskForm):
 
 
 @app.route('/signup', methods=('GET', 'POST'))
+@oidc.require_login
 def signup():
+    access_token = oidc.get_access_token()
+    if not access_token:
+        # Note(knikolla): It somehow seems that we can get through
+        # this even when there isn't an access token in the server
+        # MemoryCredentials store. Enforce a redirect and never
+        # let the user go through without a valid access token.
+        return oidc.redirect_to_auth_server(destination='/signup')
+
     form = SignUpForm()
     if form.validate_on_submit():
         r = adjutant.SignUpRequest(**form.data)
         return flask.render_template('success.html')
 
     data = {
-        'name': 'Test User',
-        'email': 'testemail@bu.edu',
-        'organization': 'Boston University'
+        'name': "%s %s" % (oidc.user_getfield('given_name'), oidc.user_getfield('family_name')),
+        'email': oidc.user_getfield('email'),
+        'organization': oidc.user_getfield('organization') if oidc.user_getfield('organization') not in [
+            'GitHub', 'Google'] else None
     }
+    # TODO(knikolla): Get affilication from id_token
+
+    logging.info(oidc.get_access_token())
+
     for key, value in data.items():
-        if key in form:
+        if key in form and value:
             form[key].render_kw = {'value': value, 'readonly': True}
 
     return flask.render_template('signup.html', form=form)
